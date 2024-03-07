a5e27d224099326af55dbf1f6c0a9812efeb1355
Kevin A. Cornelius <kcornelius@jwplayer.com>
2024-03-07 00:09:45 -0600

    Update sidebars.js
    Add 'enable-web-payments-with-stripe' to navigation

--- sidebars.js
+++ sidebars.js
@@ -42,6 +42,7 @@ module.exports = {
         'guides/syndication',
         'guides/sca',
         'guides/page-protection',
+        'guides/enable-web-payments-with-stripe',
         {
           type: 'category',
           label: 'Standalone Services',
